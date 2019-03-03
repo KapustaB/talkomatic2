befd602bbdfda7f6dc48d9f4cffc6916f792c325
Borna Kapusta <kapusta.borna.s@gmail.com>
2019-03-03 21:32:35 +0100

    config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,9 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.web_socket_server_url = "wss://action-cable-example.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://action-cable-example.herokuapp.com', 'http://action-cable-example.herokuapp.com']
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
